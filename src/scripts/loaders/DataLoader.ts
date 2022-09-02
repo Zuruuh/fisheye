dba4551a66bd683c285280c4824518ec09a41f47
Younès Ziadi <ziadi.mail.pro@gmail.com>
2022-09-02 13:21:25 +0200

    :truck: Moved all medias to media folder.

--- src/scripts/loaders/DataLoader.ts
+++ src/scripts/loaders/DataLoader.ts
@@ -1,15 +1,8 @@
-import { Media } from '../models/Media';
-import { Photographer } from '../models/Photographer';
 import { Store } from '../utils/Store';
-
-interface Data {
-  photographers: Photographer[];
-  medias: Media[];
-}
 
 export class DataLoader {
   public async load(): Promise<void> {
-    const data: Data = await (await fetch('/data/data.json')).json();
+    const data = await (await fetch('/data/data.json')).json();
 
     Object.entries(data).forEach(([key, value]) => Store.set(key, value));
   }
