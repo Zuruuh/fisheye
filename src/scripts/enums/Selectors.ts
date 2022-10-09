1f983effb837bcf1e708989986cb9a687fd239f1
Zuruuh <ziadi.mail.pro@gmail.com>
2022-10-10 00:16:51 +0200

    :sparkles: Put all selectors in a single enum to prevent their duplication

---
+++ src/scripts/enums/Selectors.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+export enum Selectors {
+  MAIN = '#app',
+  PHOTOGRAPH_HEADER = '.photograph-header',
+  MEDIA_GRID = '.media-grid',
+  MEDIA_SORTERS = '.media-sorters',
+  CONTACT_BUTTON = '#contact-button',
+  CONTACT_MODAL = '#contact-modal',
+}
