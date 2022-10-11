a2325b4f8525547b7236ba572c3f1bcb2507ea66
Zuruuh <ziadi.mail.pro@gmail.com>
2022-10-11 10:27:28 +0200

    :bug: Fix path not being found on prod

--- src/scripts/loaders/DataLoader.ts
+++ src/scripts/loaders/DataLoader.ts
@@ -2,7 +2,9 @@ import { Store } from '../utils/Store';
 
 export class DataLoader {
   public async load(): Promise<void> {
-    const data = await (await fetch('/data/data.json')).json();
+    const data = import.meta.env.DEV
+      ? await (await fetch('/data/data.json')).json()
+      : await (await fetch('./data/data.json')).json();
 
     Object.entries(data).forEach(([key, value]) => Store.set(key, value));
   }
